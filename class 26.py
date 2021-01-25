35b690a8afa2cd7f36726101b7bc3cf2bf821fc9
Abssssss1753 <77192440+Abssssss1753@users.noreply.github.com>
2021-01-25 16:16:02 +0530

    Add files via upload

---
+++ class 26.py
@@ -0,0 +1,39 @@
+########################## READ() function #################
+
+# f = open("class 25_file.txt")
+# content = f.read()
+# print(content)
+
+# f = open("class 25_file.txt")
+# for line in f:
+#     print(line)
+
+#f.close()
+
+######################### READLINE() function ###############
+# f = open("class 25_file.txt")
+# print(f.readline())
+# print(f.readline())
+# print(f.readline())
+#
+# f.close()
+
+
+######################### READLINES() function ###############
+
+# f = open("class 25_file.txt")
+# print(f.readlines())
+# f.close()
+
+################# writing to files in python   ###############
+
+# with open("python.txt","w") as f:
+#     f.write("hello python\n")
+#     f.write("my fav language is python\n")
+#
+#     f.close()
+
+################### Appending to files in python ##############
+
+# with open("python.txt","a") as f:
+#     f.write("python is very easy to understand")
